b4c7b19bf5487e2a839aa840dd69a50c26c0d669
Karugi Gitau <gkarugi@gmail.com>
2019-02-16 17:14:52 +0300

    add merchant store classes

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes(['verify' => true]);
 
-Route::group(['middleware' => 'verified'],function () {
-    Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'verified', 'prefix' => 'myDashboard', 'as' => 'dashboard.'],function () {
+    Route::group(['as' => 'admin.'], function () {
+//        Route::resource('stores', '');
+    });
 });
-
