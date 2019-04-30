72060b24ef28a89e6f4d175d878c8fcf8b74d494
Gitau Karugi <gkarugi@outlook.com>
2019-04-30 12:08:48 +0300

    fix wrong UI text box name bug

--- resources/views/dashboard/stores/create.blade.php
+++ resources/views/dashboard/stores/create.blade.php
@@ -73,7 +73,7 @@
                             </div>
                             <div class="col-md-12">
                                 <div class="form-group">
-                                    <label for="owner_phone" class="form-label">Owner E-mail</label>
+                                    <label for="owner_phone" class="form-label">Owner Phone</label>
                                     <input type="text" id="owner_phone" name="owner_phone" class="form-control {{ $errors->has('owner_phone') ? ' is-invalid' : '' }}" placeholder="Owner Phone number" value="{{ old('owner_phone') }}" required>
                                     @if ($errors->has('owner_phone'))
                                         <div class="invalid-feedback">{{ $errors->first('owner_phone') }}</div>
